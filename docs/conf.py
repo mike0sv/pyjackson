8a298f6367a94217477b075a826d18d44f0db8b1
mike0sv <mike0sv@gmail.com>
2019-11-22 16:04:16 +0300

    Bump version: 0.0.17 → 0.0.18

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ project = 'PyJackson'
 year = '2019'
 author = 'Mikhail Sveshnikov'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.17'
+version = release = '0.0.18'
 
 pygments_style = 'trac'
 templates_path = ['.']
