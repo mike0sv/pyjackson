ae52d7e57799ad5c3e23dde69fee05ea98db24f9
mike0sv <mike0sv@gmail.com>
2020-07-07 16:19:54 +0300

    Bump version: 0.0.25 → 0.0.26

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = '%s\n%s' % (re.compile('^.. start-badges.*^.. end-badges', re
 
 setup(
     name='pyjackson',
-    version='0.0.25',
+    version='0.0.26',
     license='Apache-2.0',
     description='PyJackson is a serialization library based on type hinting ',
     long_description=long_description,
